a03b5ffaf0fa5dc86c97acab30723b8a74e245ca
James.Zhang <james.zhang211@gmail.com>
2023-09-14 00:19:25 -0700

    Finished quiz

--- c-cpp/standard_calc.cpp
+++ c-cpp/standard_calc.cpp
@@ -13,7 +13,22 @@
  * @return float: The bounded angle in degrees.
  */
 float bound_to_180(float angle) {
-    return 0;
+    //Since these are local to bound_to_180, the const doesn't do much
+    //Ideally, we'd like to declare these separately, in a namespace, and possibly in a header file
+    const float angle_bound {180.0f};
+    const float angle_increment {360.0f};
+
+    //The angle parameter can only satisfy one of the while loop ocnditions 
+    //So structuring the while loops like this gives us an implicit if, else
+    while (angle >= angle_bound)
+        angle -= angle_increment;   //Subtract increments of 360 degrees
+                                    //Allows us to deal with angles > 360, and angles between [180, 360]
+                                    //in the same line
+    
+    while (angle < -angle_bound)
+        angle += angle_increment;
+
+    return angle;
 }
 
 /**
@@ -28,6 +43,29 @@ float bound_to_180(float angle) {
  * @param second_angle: The second bounding angle in degrees.
  * @return bool: TRUE when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, FALSE otherwise
  */
+
+/**
+*ASSUMPTIONS
+* I apologize, as I don't quite understand the phrase: TRUE when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`
+* As such, I've made the following assumption for what the function should do:
+* Find out if the middle angle is between the 1st and 2nd angle, provided all angles are bound between [-180, 180) degrees
+*/
 bool is_angle_between(float first_angle, float middle_angle, float second_angle) {
-    return true;
+    //Ideally we'd like to pass the angle to bound_to_180 by reference to avoid creating extra variables 
+    //But we'll utilize bound_to_180 having a return value
+
+    //Bind angles between [-180, 180) degrees, as per the assumption
+    float bound_first_angle {bound_to_180(first_angle)};
+    float bound_middle_angle {bound_to_180(middle_angle)};
+    float bound_second_angle {bound_to_180(second_angle)};
+
+    //Determine if first_angle or second_angle is the lower bound
+    //Depending on the result, check that middle_angle is greater than the lower bound (could be first or second_angle)
+    //And that middle_angle is less than upper bound
+    if (bound_first_angle < second_angle){
+        return (bound_first_angle < bound_middle_angle && bound_middle_angle < bound_second_angle);
+    }
+    else {
+        return (bound_first_angle > bound_middle_angle && bound_middle_angle > bound_second_angle);
+    }
 }
--- c-cpp/test_standard_calc.cpp
+++ c-cpp/test_standard_calc.cpp
@@ -1,15 +1,50 @@
 #include "stdio.h"
 #include "cutest/CuTest.h"
 #include "standard_calc.h"
+#include "cutest/CuTest.c" //Due to how my gcc is setup, this is the only way I could get the project to build
+                            //Without complaining about missing dependencies in CuTest. I apologize if this violates the rules
 
 /**************** Tests for bound_to_180() *******************/
 void test_bound_basic1(CuTest *tc) {
+    //Test angles already between bounds
     CuAssertDblEquals(tc, 0, bound_to_180(0), 0.0001);
+    CuAssertDblEquals(tc, 90, bound_to_180(90), 0.0001);
+    CuAssertDblEquals(tc, -90.1, bound_to_180(-90.1), 0.0001);
+    CuAssertDblEquals(tc, -180, bound_to_180(-180), 0.0001);
+
+    //Test angles outside bounds
+    CuAssertDblEquals(tc, -180, bound_to_180(180), 0.0001);
+    CuAssertDblEquals(tc, -89.734985, bound_to_180(270.265), 0.0001);
+    CuAssertDblEquals(tc, 90, bound_to_180(-270), 0.0001);
+    CuAssertDblEquals(tc, 0, bound_to_180(360), 0.0001);
+    CuAssertDblEquals(tc, 0, bound_to_180(-360), 0.0001);
+
+    //Test angles >|360|
+    CuAssertDblEquals(tc, -56, bound_to_180(1024), 0.0001);
+    CuAssertDblEquals(tc, 55.9999, bound_to_180(-1024.0001), 0.0001);
+
+    //Test passing an int still returns a flaot
+    CuAssertDblEquals(tc, 60.0f, bound_to_180(static_cast<int>(420)), 0.0001);
 }
 
 /**************** Tests for is_angle_between() *******************/
 void test_between_basic1(CuTest *tc) {
+    //Test for middle angle between bounds
     CuAssertTrue(tc, is_angle_between(0, 1, 2));
+    CuAssertTrue(tc, is_angle_between(0, 45, 90));
+
+    //Middle angle outside bounds
+    CuAssertIntEquals(tc, 0, is_angle_between(45, 90, 270)); //Since CuTest can't check false, I compared an implicit int cast of false to 0
+
+    //Bounds are equal (ideally this would throw a separate assert in the func, but unfortunately it can't be implemented without access to <assert>)
+    CuAssertIntEquals(tc, 0, is_angle_between(90, 90, 90));
+
+    //Test for angles that are negative, >|360|, and have decimals
+    CuAssertTrue(tc, is_angle_between(-112, -90, -1));
+    CuAssertIntEquals(tc, 0, is_angle_between(6969, 4200, 1984));
+    CuAssertTrue(tc, is_angle_between(-420, -59, 1382));
+    CuAssertTrue(tc, is_angle_between(25.001, 25.002, 25.003));
+
 }
 
 int main(int argc, char const *argv[]) {
